218a38a8e5ad0e73d8dedcad143be07550228a17
marco-vignozzi <marcovignozzi7@gmail.com>
2024-05-12 12:25:24 +0200

    firstCommut

---
+++ frontend/src/App.js
@@ -0,0 +1,12 @@
+import Table from './components/table/Table'
+
+function App() {
+  return (
+    // Commento a caso
+    <div className="App">
+      <Table />
+    </div>
+  );
+}
+
+export default App;
