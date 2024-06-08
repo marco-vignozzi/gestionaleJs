d17631725fc97f1ee99568469c648ad30a262d58
marco-vignozzi <marcovignozzi7@gmail.com>
2024-06-08 23:04:55 +0200

    aggiunto controllo navbar

--- frontend/src/components/navbar/Navbar.js
+++ frontend/src/components/navbar/Navbar.js
@@ -3,24 +3,26 @@ import { useNavigate } from 'react-router-dom';
 
 export default function Navbar(props) {
     const { options } = props;
-
     const navigate = useNavigate();
+
     return (
         <div className="navbar">
-            {options?.map((el, i) => {
-                return (
-                    <button
-                        key={`navbar-btn-${i}`}
-                        className="navbar-btn"
-                        onClick={() => {
-                            el.onClick && el.onClick();
-                            navigate(el.path);
-                        }}
-                    >
-                        <span>{el.label}</span>
-                    </button>
-                );
-            })}
+            {options
+                ? options?.map((el, i) => {
+                      return (
+                          <button
+                              key={`navbar-btn-${i}`}
+                              className="navbar-btn"
+                              onClick={() => {
+                                  el.onClick && el.onClick();
+                                  navigate(el.path);
+                              }}
+                          >
+                              <span>{el.label}</span>
+                          </button>
+                      );
+                  })
+                : null}
         </div>
     );
 }
